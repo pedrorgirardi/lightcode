ac315a3db2ac650628123dfdee9576701f63ab53
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-05-19 18:01:58 +0700

    'sys' map

--- src/lightcode/core.cljs
+++ src/lightcode/core.cljs
@@ -2,7 +2,8 @@
   (:require 
    ["vscode" :as vscode]
    [datascript.core :as d]
-   [lightcode.cmd :as cmd]))
+   [lightcode.cmd :as cmd]
+   [lightcode.out :as out]))
 
 
 (defn ClojureLanguageConfiguration []
@@ -12,9 +13,9 @@
                                                 :decreaseIndentPattern nil})))
 
 
-(defn- register-command [ctx cmd]
+(defn- register-command [sys cmd]
   (-> (.-commands vscode)
-      (.registerCommand (-> cmd meta :cmd) #(cmd ctx))))
+      (.registerCommand (-> cmd meta :cmd) #(cmd sys))))
                  
                  
 (defn- register-disposable [^js context ^js disposable]
@@ -22,8 +23,8 @@
       (.push disposable)))
 
                  
-(defn- cmd-setup [^js context ctx cmd]
-  (->> (register-command ctx cmd)
+(defn- cmd-setup [^js context sys cmd]
+  (->> (register-command sys cmd)
        (register-disposable context)))
                  
 
@@ -32,10 +33,16 @@
       (.setLanguageConfiguration "clojure" (ClojureLanguageConfiguration.)))
   
   (let [conn (d/create-conn {})
-        ctx  {:conn conn}]
-   (cmd-setup context ctx #'cmd/welcome))
-  
-  (js/console.log "Light Code is active."))
+        
+        ^js out (-> (.-window vscode)
+                    (.createOutputChannel "Light Code"))
+        
+        sys {:conn conn
+             :out  out}]
+    
+    (cmd-setup context sys #'cmd/welcome)
+    
+    (out/append-line out "Light Code is active.")))
 
 
 (defn deactivate []
---
+++ src/lightcode/out.cljs
@@ -0,0 +1,14 @@
+(ns lightcode.out)
+
+
+(defn append-line
+  "Append the given value and a line feed character to the channel."
+   [^js output value]
+  (.appendLine output value))
+
+
+(defn append-line-and-show
+  "Append the given value and a line feed character to the channel and reveals it."
+   [^js output value]
+  (.appendLine output value)
+  (.show output false))
