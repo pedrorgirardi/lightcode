a3370f599a134b23c20a2810d92e3edf3820e488
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-06-15 17:49:33 +0700

    Send Selection to REPL command

--- src/lightcode/core.cljs
+++ src/lightcode/core.cljs
@@ -2,16 +2,47 @@
   (:require
    [vscode :as vscode]
    [vscode-languageclient :as vscode-languageclient]
+   [cljs.reader :as edn]
 
+   [lightcode.workspace :as workspace]
    [lightcode.editor :as editor]
+   [lightcode.document :as document]
+   [lightcode.out :as out]
    [lightcode.op :as op]))
+
+
+(def *sys
+  (atom {}))
+
 
 ;; ------------------------------------------------
 ;; HELPERS
 ;; ------------------------------------------------
 
 (defn- register-command [cmd]
-  (vscode/commands.registerCommand (-> cmd meta :cmd) cmd))
+  (let [cmd-name (-> cmd meta :cmd)
+        callback (fn []
+                   (js/console.log (str "Run command '" cmd-name "'"))
+
+                   (try
+                     (cmd)
+                     (catch js/Error e
+                       (js/console.error (str "Failed to run command '" cmd-name "'") e))))]
+
+    (vscode/commands.registerCommand cmd-name callback)))
+
+
+(defn- register-text-editor-command [cmd]
+  (let [cmd-name (-> cmd meta :cmd)
+        callback (fn [editor edit args]
+                   (js/console.log (str "Run editor command '" cmd-name "'"))
+
+                   (try
+                     (cmd editor edit args)
+                     (catch js/Error e
+                       (js/console.error (str "Failed to run editor command '" cmd-name "'") e))))]
+
+    (vscode/commands.registerTextEditorCommand cmd-name callback)))
 
 
 (defn- register-disposable [^js context ^js disposable]
@@ -19,34 +50,40 @@
       (.push disposable)))
 
 
-(defn- reg-cmd [^js context cmd]
-  (->> (register-command cmd)
-       (register-disposable context)))
-
 
 ;; ------------------------------------------------
 ;; COMMANDS
 ;; ------------------------------------------------
 
-(defn ^{:cmd "lightcode.loadFile"} cmd-load-file []
-  (when-let [document (editor/active-clojure-document)]
-    (op/load-file! (:lc.document/env document)
-                   (:lc.document/content document)
-                   (:lc.document/src-path-relative-path document)
-                   (:lc.document/file-name document)))
+(defn ^{:cmd "lightcode.sendSelectionToREPL"} send-selection-to-repl [editor edit args]
+  (let [language-client (get @*sys :language-client)
+        document        (.-document editor)
+        selection       (.-selection editor)
+        range           (vscode/Range. (.-start selection) (.-end selection))
+        selected-text   (.getText document range)
+        message         {:op      "eval"
+                         :code    selected-text
+                         :context (merge
+                                   {:env   (document/file-language-extension document)
+                                    :nrepl {:port (workspace/nrepl-port!)}}
+                                   (workspace/cljs-repl-context!))}]
+
+    (.then (.sendRequest language-client "repl" (pr-str message))
+           (fn [result]
+             (let [output-channel (get @*sys :output-channel)
+                   out-str (str "\n" selected-text "\n=> " result "\n=> " (-> (edn/read-string result) :value first))]
+               (out/append-line-and-show output-channel out-str)))))
 
   nil)
 
 
-(defn ^{:cmd "lightcode.loadNamespaces"} cmd-load-namespaces []
-  (when-let [document (editor/active-clojure-document)]
-    (op/ns-load-all! (editor/env document)))
-
-  nil)
+;; ------------------------------------------------
 
 
 (defn activate [^js context]
-  (let [server-options {:run   {:command "lightcode"}
+  (let [output-channel (vscode/window.createOutputChannel "Light Code")
+
+        server-options {:run   {:command "lightcode"}
                         :debug {:command "bash"
                                 :args    ["-c" "cd /Users/pedro/Developer/lightcode.server && lein run"]}}
 
@@ -58,9 +95,15 @@
 
     (.start language-client)
 
-    (register-disposable context language-client))
+    (register-disposable context language-client)
 
-  (js/console.log "Light Code is active."))
+    (->> (register-text-editor-command  #'send-selection-to-repl)
+         (register-disposable context))
+
+    (reset! *sys  {:output-channel  output-channel
+                   :language-client language-client})
+
+    (out/append-line output-channel "Light Code is active. Happy coding!")))
 
 
 (defn deactivate []
