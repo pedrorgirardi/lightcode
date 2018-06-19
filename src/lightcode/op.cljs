cd39f1997661f34c55b66f214cda450c29e200fb
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-06-19 09:29:24 +0700

    Delete unused code

--- src/lightcode/op.cljs
+++ src/lightcode/op.cljs
@@ -1,20 +1,10 @@
 (ns lightcode.op
   (:require
-   [axios :as axios]
-
    [kitchen-async.promise :as p]
+
    [lightcode.config :as config]
    [lightcode.editor :as editor]
    [lightcode.workspace :as workspace]))
-
-
-(defn send! [env message]
-  (let [context {:context (merge {:nrepl {:port (editor/nrepl-port!)}
-                                  :env   env}
-
-                                 (workspace/cljs-repl-context!))}
-        message (merge message context)]
-    (.post axios config/repl-api-url (clj->js message))))
 
 
 (defn ->info
@@ -26,14 +16,6 @@
    :symbol symbol})
 
 
-(defn info!
-  "Sends an 'info' message.
-  
-  `ns` Namespace name, e.g. lightcode.op
-  `symbol` Symbol, e.g. send!"
-  [env ns symbol]
-  (send! env (->info ns symbol)))
-
 (defn ->eldoc
   "`ns` Namespace name, e.g. lightcode.op
    `symbol` Symbol, e.g. send!"
@@ -41,15 +23,6 @@
   {:op     "eldoc"
    :ns     ns
    :symbol symbol})
-
-
-(defn eldoc!
-  "Sends an 'eldoc' message.
-  
-  `ns` Namespace name, e.g. lightcode.op
-  `symbol` Symbol, e.g. send!"
-  [env ns symbol]
-  (send! env (->eldoc ns symbol)))
 
 
 (defn ->load-file
@@ -62,28 +35,12 @@
    :file-path path
    :file-name name})
 
-(defn load-file!
-  "`content` Full contents of a file of code
-   `path` Source-path-relative path of the source file, e.g. clojure/java/io.clj
-   `name` Name of source file, e.g. io.clj"
-  [env content path name]
-  (send! env (->load-file content path name)))
-
 
 (defn ->ns-vars
   "`ns` Namespace name, e.g. lightcode.op"
   [ns]
   {:op "ns-vars"
    :ns ns})
-
-
-(defn ns-vars!
-  "Sends a 'ns-vars' message.
-  
-  `ns` Namespace name, e.g. lightcode.op
-  `symbol` Symbol, e.g. send!"
-  [env ns]
-  (send! env (->ns-vars ns)))
 
 
 (defn ->ns-vars-with-meta
@@ -93,20 +50,5 @@
    :ns ns})
 
 
-(defn ns-vars-with-meta!
-  "Sends a 'ns-vars-with-meta' message.
-  
-  `ns` Namespace name, e.g. lightcode.op
-  `symbol` Symbol, e.g. send!"
-  [env ns]
-  (send! env (->ns-vars-with-meta ns)))
-
-
 (defn ->ns-load-all []
   {:op "ns-load-all"})
-
-
-(defn ns-load-all!
-  "Sends a 'ns-load-all' message."
-  [env]
-  (send! env (->ns-load-all)))
